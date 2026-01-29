b2a8b9d56073b3a683c49f09828ecece137706b0
angleegoh <gohanglee@outlook.com>
2026-01-29 09:52:16 +0800

    Introduce Lego

---
+++ src/main/java/Lego.java
@@ -0,0 +1,11 @@
+public class Lego {
+    public static void main(String[] args) {
+        String text = "____________________________________________________________\n"
+                + " Hello! I'm [Lego]\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+        System.out.println(text);
+    }
+}
