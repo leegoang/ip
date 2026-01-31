a7d8ad8be7f845e793c806f3a84a02157721126a
leegoang <E1398598@u.nus.edu>
2026-01-31 18:28:27 +0800

    Echo function

--- src/main/java/Lego.java
+++ src/main/java/Lego.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Lego {
     public static void main(String[] args) {
-        String text = "____________________________________________________________\n"
-                + " Hello! I'm [Lego]\n"
+        String line;
+        Scanner in = new Scanner (System.in);
+        Boolean isRunning = true;
+
+        String openingText = " Hello! I'm [Lego]\n"
                 + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
-        System.out.println(text);
+                + " Type something and I may just copy you! :D";
+        String closingText = " Bye. Hope to see you again soon!";
+
+        System.out.println(openingText);
+        while (isRunning) {
+            line = in.nextLine();
+            if (line.toLowerCase().equals("bye")) {
+                isRunning = false;
+                System.out.println(closingText);
+            } else {
+                System.out.println(line);
+            }
+        }
+        in.close();
     }
 }
