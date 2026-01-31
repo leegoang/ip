9902fcfa036552abbea26c6884d817214801f00d
leegoang <E1398598@u.nus.edu>
2026-01-31 18:51:48 +0800

    Add List and Add function

--- src/main/java/Lego.java
+++ src/main/java/Lego.java
@@ -5,10 +5,12 @@ public class Lego {
         String line;
         Scanner in = new Scanner (System.in);
         Boolean isRunning = true;
+        String[] wordList = new String[100];
+        int wordNum = 0;
 
         String openingText = " Hello! I'm [Lego]\n"
                 + " What can I do for you?\n"
-                + " Type something and I may just copy you! :D";
+                + " Type something and I will record it for you! :D";
         String closingText = " Bye. Hope to see you again soon!";
 
         System.out.println(openingText);
@@ -17,8 +19,15 @@ public class Lego {
             if (line.toLowerCase().equals("bye")) {
                 isRunning = false;
                 System.out.println(closingText);
+            } 
+            else if (line.toLowerCase().equals("list")) {
+                for (int i = 0; i < wordNum; i++) {
+                    System.out.println(" " + Integer.toString(i + 1) + ". " + wordList[i]);
+                }
             } else {
-                System.out.println(line);
+                System.out.println(" added: " + line);
+                wordList[wordNum] = line;
+                wordNum++;
             }
         }
         in.close();
