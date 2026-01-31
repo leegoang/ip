fc5543d1aaa4ae8b9c267e82c306ca6b1aad5f84
leegoang <E1398598@u.nus.edu>
2026-01-31 19:46:12 +0800

    Format to Coding Standard

--- src/main/java/Lego.java
+++ src/main/java/Lego.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class Lego {
     public static void main(String[] args) {
         String line;
-        Scanner in = new Scanner (System.in);
+        Scanner in = new Scanner(System.in);
         Boolean isRunning = true;
         String[] wordList = new String[100];
         Boolean[] isCheckedList = new Boolean[100];
@@ -25,20 +25,17 @@ public class Lego {
             if (command.toLowerCase().equals("bye")) {
                 isRunning = false;
                 System.out.println(closingText);
-            } 
-            else if (command.toLowerCase().equals("list")) {
+            } else if (command.toLowerCase().equals("list")) {
                 for (int i = 0; i < wordNum; i++) {
-                    System.out.print(" "+ Integer.toString(i + 1) + ".");
+                    System.out.print(" " + Integer.toString(i + 1) + ".");
                     printListLine(i, isCheckedList, wordList);
                 }
-            } 
-            else if (command.toLowerCase().equals("mark")) {
+            } else if (command.toLowerCase().equals("mark")) {
                 int itemIndex = Integer.parseInt(splitCommand[1]) - 1;
                 isCheckedList[itemIndex] = true;
                 System.out.println("Nice! I've marked this task as done:");
                 printListLine(itemIndex, isCheckedList, wordList);
-            } 
-            else if (command.toLowerCase().equals("unmark")) {
+            } else if (command.toLowerCase().equals("unmark")) {
                 int itemIndex = Integer.parseInt(splitCommand[1]) - 1;
                 isCheckedList[itemIndex] = false;
                 System.out.println("OK, I've marked this task as not done yet:");
@@ -58,8 +55,9 @@ public class Lego {
         }
         return " ";
     }
+
     public static void printListLine(int num, Boolean[] isCheckedList, String[] wordList) {
-        System.out.println(" [" + applyTick(isCheckedList[num]) + "] " 
-                    + wordList[num]);
+        System.out.println(" [" + applyTick(isCheckedList[num]) + "] "
+                + wordList[num]);
     }
 }
