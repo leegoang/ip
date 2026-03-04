8a5845ef5bc7a046ecf0d477fc56cf7cbf9e2104
leegoang <E1398598@u.nus.edu>
2026-03-04 19:38:07 +0800

    Add JavaDoc comments

--- src/main/java/lego/database/DatabaseHandler.java
+++ src/main/java/lego/database/DatabaseHandler.java
@@ -13,55 +13,29 @@ import lego.task.Todo;
 
 import java.util.ArrayList;
 
+/**
+ * Handles file I/O operations for persisting and loading tasks.
+ * 
+ * Manages reading tasks from a file and writing tasks to a file to ensure
+ * data persistence across application sessions.
+ */
 public class DatabaseHandler {
-    private String filePath = "../data/lego.txt";
 
+    /**
+     * Constructs a DatabaseHandler with the specified file path.
+     * 
+     * @param filePath the path to the file where tasks are stored
+     */
     public DatabaseHandler(String filePath) {
         this.filePath = filePath;
     }
 
-    public String getFilePath() {
-        return this.filePath;
-    }
-
-    public void writeToFile(String textToAdd) throws IOException {
-        File file = new File(this.filePath);
-        file.getParentFile().mkdirs();
-        FileWriter fw = new FileWriter(this.filePath);
-        fw.write(textToAdd);
-        fw.close();
-    }
-
-    public void appendToFile(String textToAppend) throws IOException {
-        FileWriter fw = new FileWriter(this.filePath, true);
-        fw.write(textToAppend);
-        fw.close();
-    }
-
-    public void saveFileContents(ArrayList<Task> taskList) throws IOException {
-        String lineEntry = "";
-        for (Task task : taskList) {
-            String taskType = "";
-            if (task instanceof Todo) {
-                taskType = "T";
-            } else if (task instanceof Deadline) {
-                taskType = "D";
-            } else if (task instanceof Event) {
-                taskType = "E";
-            }
-            String isDone = task.isDone() ? "1" : "0";
-            lineEntry += taskType + " | " + isDone + " | " + task.getDescription();
-            if (task instanceof Deadline) {
-                lineEntry += " | " + ((Deadline) task).getBy();
-            } else if (task instanceof Event) {
-                lineEntry += " | " + ((Event) task).getFrom() + " | " + ((Event) task).getTo();
-            }
-            lineEntry += "\n";
-        }
-        this.writeToFile(lineEntry);
-
-    }
-
+    /**
+     * Loads task data from the file.
+     * 
+     * @return a list of tasks loaded from the file
+     * @throws FileNotFoundException if the specified file does not exist
+     */
     public ArrayList<Task> loadFileContents() throws FileNotFoundException {
         File f = new File(this.filePath);
         Scanner s = new Scanner(f);
@@ -97,4 +71,52 @@ public class DatabaseHandler {
         return taskList;
     }
 
+    /**
+     * Saves the provided tasks to the file.
+     * 
+     * @param tasks the list of tasks to save
+     */
+    public void saveToFile(ArrayList<Task> tasks) throws IOException {
+        String lineEntry = "";
+        for (Task task : tasks) {
+            String taskType = "";
+            if (task instanceof Todo) {
+                taskType = "T";
+            } else if (task instanceof Deadline) {
+                taskType = "D";
+            } else if (task instanceof Event) {
+                taskType = "E";
+            }
+            String isDone = task.isDone() ? "1" : "0";
+            lineEntry += taskType + " | " + isDone + " | " + task.getDescription();
+            if (task instanceof Deadline) {
+                lineEntry += " | " + ((Deadline) task).getBy();
+            } else if (task instanceof Event) {
+                lineEntry += " | " + ((Event) task).getFrom() + " | " + ((Event) task).getTo();
+            }
+            lineEntry += "\n";
+        }
+        this.writeToFile(lineEntry);
+
+    }
+
+    public String getFilePath() {
+        return this.filePath;
+    }
+
+    public void writeToFile(String textToAdd) throws IOException {
+        File file = new File(this.filePath);
+        file.getParentFile().mkdirs();
+        FileWriter fw = new FileWriter(this.filePath);
+        fw.write(textToAdd);
+        fw.close();
+    }
+
+    public void appendToFile(String textToAppend) throws IOException {
+        FileWriter fw = new FileWriter(this.filePath, true);
+        fw.write(textToAppend);
+        fw.close();
+    }
+
+    private String filePath;
 }
